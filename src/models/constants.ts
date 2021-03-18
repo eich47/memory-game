d98b72fb6a2628c669ef2d9fb9d7816b329ebfd3
eich47 <eich@tut.by>
2021-03-18 12:43:51 +0300

    fix: correct constants of color

--- src/models/constants.ts
+++ src/models/constants.ts
@@ -3,7 +3,7 @@ import { Colors } from "./Types";
 export const colors: Colors = {
   COLOR1: "#00ffff",
   COLOR2: "#c0c0c0",
-  COLOR3: "#008000f",
+  COLOR3: "#008000",
   COLOR4: "#808000",
   COLOR5: "#008080",
   COLOR6: "#0000ff",
