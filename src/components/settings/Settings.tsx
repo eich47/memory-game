39124e542cddf9aa142b20fade785d2d8ab6d64b
eich47 <eich@tut.by>
2021-03-22 14:25:33 +0300

    feat: add additional button to close the menu of settings

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,5 +1,7 @@
 import React, { useContext } from "react";
+import { AppContext } from "../../context/AppContext";
 import { SettingsContext } from "../../context/SettingsContext";
+import ToggleMenu from "../buttons/ToggleMenu";
 import CardNumber from "./cards-number/CardNumber";
 import "./Settings.css";
 
@@ -9,9 +11,15 @@ const Settings = () => {
     handleChangeCarsNumber,
     messageCarsNumberError,
   } = useContext(SettingsContext);
+
+  const { isOpenMenu, toggleMenu } = useContext(AppContext);
   return (
     <div className="settings">
-      <span className="settings__title">Настройки</span>
+      <div className="settings__container">
+        <span className="settings__title">Настройки</span>
+        <ToggleMenu isOpenMenu={isOpenMenu} handlerClick={toggleMenu} />
+      </div>
+
       <CardNumber
         cardsNumber={cardsNumber}
         onChangeCarsNumber={handleChangeCarsNumber}
