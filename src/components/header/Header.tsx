a7614d868cdf35065b226f49397f4de0e6a2ceff
eich47 <eich@tut.by>
2021-03-23 15:07:12 +0300

    feat: add animation for the settings menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -7,6 +7,7 @@ import Score from "../score/Score";
 import ToggleMenu from "../buttons/ToggleMenu";
 import SettingsProvider from "../../context/SettingsContext";
 import Settings from "../settings/Settings";
+import { CSSTransition } from "react-transition-group";
 
 const Header = () => {
   const { gameState, isOpenMenu, toggleMenu } = useContext(AppContext);
@@ -24,11 +25,17 @@ const Header = () => {
           <div className="header__column3">
             <ToggleMenu isOpenMenu={isOpenMenu} handlerClick={toggleMenu} />
           </div>
-          {isOpenMenu && (
+
+          <CSSTransition
+            in={isOpenMenu}
+            timeout={{ enter: 500, exit: 100 }}
+            classNames="settings-animate"
+            unmountOnExit
+          >
             <SettingsProvider>
               <Settings />
             </SettingsProvider>
-          )}
+          </CSSTransition>
         </div>
       </div>
     </header>
